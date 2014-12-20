2634811c036c41bea11ba9e8adbe7198206ff867
SpamBaconEggs <spam.bacon.eggs@gmail.com>
2014-12-20 19:31:40 +0000

    Refactor. Cosmetic only.
    Moved around various sections and removed commented/defunct
    configuration code.

--- cedet_config.el
+++ cedet_config.el
@@ -6,16 +6,7 @@
 ;; (customize-set-variable auto-save-default nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Keyboard shortcuts
-(global-set-key "\M-g" 'goto-line)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Editing basics
-(delete-selection-mode 1)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;<package manager>
 ;; Setting up package manager bits, as per http://www.logilab.org/173886
 
 ;; this is intended for manually installed libraries
@@ -28,61 +19,98 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-;; Install a hook running post-init.el *after* initialization took place
-;; (add-hook 'after-init-hook (lambda () (load "post-init.el")))
-
-;; Do here basic initialization, (require) non-ELPA packages, etc.
+;; Initialize and require non-ELPA packages, etc.
 
 ;; disable automatic loading of packages after init.el is done
 (setq package-enable-at-startup nil)
 ;; and force it to happen now
 (package-initialize)
-;; NOW you can (require) your ELPA packages and configure them as normal
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; now you can (require) your ELPA packages and configure them as normal
+
+;;</package manager>
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;<navigation>
+
+;; Go to specified line number
+(global-set-key "\M-g" 'goto-line)
+
+;;Easy window and buffer switching
+
+;; Switch windows
+;; see windmove here http://ergoemacs.org/emacs/emacs_winner_mode.html
+(require 'windmove)
+(global-set-key (kbd "<s-left>") 'windmove-left)
+(global-set-key (kbd "<s-right>") 'windmove-right)
+(global-set-key (kbd "<s-down>") 'windmove-down)
+(global-set-key (kbd "<s-up>") 'windmove-up)
+
+;; Swap buffers
+;; see http://stackoverflow.com/questions/1774832/how-to-swap-the-buffers-in-2-windows-emacs
+;; and http://www.emacswiki.org/emacs/TransposeFrame
+(add-to-list 'load-path
+             "~/.emacs.d/extra")
+(require 'transpose-frame)
+;;</navigation>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;<editing basics>
+
+;;Delete selection when calling yank/pasting (default is to annoyingly
+;;unselect and then yank inserts instead of overwrites)
+(delete-selection-mode 1)
+
+;;Line numbers
+(require 'linum)
+(setq linum-mode t)
+;;</editing basics>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;<ido>
 ;; ido - nice quick buffer option completion
 (require 'ido)
 ;;(setq ido-enable-flex-matching t)
 (ido-mode t)
 (setq ido-everywhere t)
+
 ;; ido-ubiquitous - Does what you were really hoping for when you did
 ;;(require 'ido-ubiquitous)
 ;;(setq ido-ubiquitous-mode t)
+;;</ido>
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; editing/viewing of markdown files
-;; use git-hub-flavoured markdown instead of standard
-(autoload 'markdown-mode "markdown-mode"
-  "Major mode for editing Markdown files" t)
-;; standard markdown flavour is below
-;;(autoload 'markdown-mode "markdown-mode"
-;;  "Major mode for editing Markdown files" t)
-;;; Markdown mode, choosing the github-flavoured-mode, as that gives
-;;; syntax highlighting. Woot!
-(add-to-list 'auto-mode-alist '("\\.text\\'" . gfm-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
-;; see http://stackoverflow.com/a/18443287
-(setq markdown-command "markdown2 -x fenced-code-blocks -x tables")
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;<cscope tags>
+;;xscope.el for source code browsing, jumping to definitions, etc.
+;;see http://stackoverflow.com/a/12923994/601626
+;;and https://github.com/dkogan/xcscope.el
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; xscope.el for source code browsing, jumping to definitions, etc.
-;; see http://stackoverflow.com/a/12923994/601626
-;; and https://github.com/dkogan/xcscope.el
 (add-to-list 'load-path
              "~/.emacsecb.d/xcscope")
 (require 'xcscope)
 (cscope-setup)
+
 ;; keybinding for symbol finding
 (global-set-key (kbd "<f1>") 'cscope-find-this-symbol)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;</cscope tags>
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;<ggtags>
+;; ggtags - for GNU Global tags and Exuberant C Tags (depending on how GNU GLOBAL was compiled)
+(require 'ggtags)
+;;   enable ggtags for c modes
+(add-hook 'c-mode-common-hook
+              (lambda ()
+                (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+                  (ggtags-mode 1))))
+;;(require 'ctags)
+;; Bind cedet navigation window to right-mouse button
+;;(global-cedet-m3-minor-mode 1)
+;;</gtags>
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;<python>
 ;; Elpy for Python goodies
 ;; See: https://github.com/jorgenschaefer/elpy/wiki/Installation
 
@@ -96,23 +124,10 @@
 ;; Use Jedi instead. (WIP)
 ;;(add-hook 'python-mode-hook 'jedi:setup)
 ;;(setq jedi:complete-on-dot t)
+;;</python>
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;(add-to-list 'load-path
-;;             "~/.emacsecb.d/cedet-bzr/trunk")
-;;(require 'cedet-devel-load)
-(require 'cedet)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Personal preferences
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;(set-specifier top-toolbar-visible-p nil)
-;;(setq top-toolbar-visible-p nil)
-;;Line numbers
-(require 'linum)
-(setq linum-mode t)
-
+;;<code mode preferences>
 ;;Use spaces instead of tabs
 (setq-default indent-tabs-mode nil)
 ;;Use 4 spaces in c mode
@@ -124,15 +139,14 @@
 (add-hook 'prog-mode-hook '(lambda ()
                              (local-set-key (kbd "RET") 'newline-and-indent)))
 ;;Guess the default offset in files automatically
-(add-to-list 'load-path
-             "~/.emacsecb.d/dtrt-indent")
 (require 'dtrt-indent)
 (dtrt-indent-mode 1)
 
+;;</code mode preferences>
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Highlight whitespace issues
+;;<whitespace handling>
 (require 'whitespace)
 (autoload 'whitespace-mode           "whitespace" "Toggle whitespace visualization."        t)
 (autoload 'whitespace-toggle-options "whitespace" "Toggle local `whitespace-mode' options." t)
@@ -143,10 +157,10 @@
 ;;working on code/diffs.
 (require 'ws-trim)
 (global-ws-trim-mode t)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;</whitespace handling>
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Autocompletion - basic setup
+;;<code autocompletion>
 (require 'auto-complete)
 ;; (add-to-list 'ac-dictionary-directories "~/.emacsecb.d/ac-dict")
 (require 'auto-complete-config)
@@ -161,15 +175,23 @@
 ;;Seems to hook up to semantic automatically, so C/C++ autocompletion
 ;;works at this point.
 (require 'ac-python)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;</code autocompletion>
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;directory and module browsing using ecb and cedet
-;;Following http://altom.ro/blog/emacs-python-ide-recipe
+;;<cedet ede>
+;;cedet is massive. It provides various tools useful for building up an
+;;IDE. E.g. EDE, for setting up directories and files that are part projects
+;;(used heavily by ECB), semantic (for getting tags/symbols from code,
+;;jumping to definitions - although mileage here is very variable).
+;;This is one approach to get emacs to be more like a fully-fledged
+;;modern IDE. The other is projectile (haven't added it to the config yet).
+;;See for example,
+;;http://alexott.net/en/writings/emacs-devenv/EmacsCedet.html
+;;and http://stackoverflow.com/questions/12711765/status-of-cedet-and-ecb-in-emacs-24-2/12716443#12716443
+;;and http://altom.ro/blog/emacs-python-ide-recipe
 
-;; cedet - coding environment for emacs
-;;(add-to-list 'load-path
-;;             "/home/fanner/EmacsIDE/cedet-bzr/trunkecb")
+(require 'cedet)
 (require 'cedet-cscope)
 
 ;; ede - project management which is part of cedet
@@ -238,14 +260,15 @@
   (message "skipping over an ede project because project dir does not exist")
   )
 
-;; See the notes in cedet-cscope-tips.md on getting case-insensitive
-;; cscope searching to work.
+;;See the notes in cedet-cscope-tips.md on getting case-insensitive
+;;cscope searching to work.
 (defun my-find-file-in-project (filesubstring)
   "EDE's file finding is a bit pants. It doesn't make use of previously
    hashed/searched results correctly, and then doesn't give you the option
    to choose between multiple similarly named files (just picks the first).
    So this is an attempt to do a bit better by using cedet-cscope and ido
    directly."
+  ;;todo - make this independent of cedet, so we only rely on xcscope?
   (interactive "sFind file: ")
   (require 'cedet-cscope)
   (require 'ido)
@@ -258,37 +281,6 @@
 )
 
 (global-set-key (kbd "C-o") 'my-find-file-in-project)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;; ecb - emacs code browser (uses cedet)
-(require 'ecb)
-
-;; ggtags - for GNU Global tags and Exuberant C Tags (depending on how GNU GLOBAL was compiled)
-(require 'ggtags)
-;;   enable ggtags for c modes
-(add-hook 'c-mode-common-hook
-              (lambda ()
-                (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
-                  (ggtags-mode 1))))
-;;(require 'ctags)
-;; Bind cedet navigation window to right-mouse button
-;;(global-cedet-m3-minor-mode 1)
-;; Following the ecb README from the source directory
-;;(require 'ecb-autoloads) ;; Switched off if you want to speed-up startup
-
-(setq stack-trace-on-error nil) ;;don’t popup Backtrace window
-(setq ecb-tip-of-the-day nil)
-;;(setq ecb-auto-activate t)
-;;(setq ecb-layout-name "left6")
-;;(setq ecb-options-version "2.40")
-;;(setq ecb-primary-secondary-mouse-buttons (quote mouse-1–mouse-2))
-;;(setq ecb-source-path (quote ("~/")))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;C/C++ Source Browsing, Completion, Etc. (ECB, using CEDET, using Semantic
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;Using cscope for searching for symbols works reasonably well on my
 ;;C/C++ projects.  Semantic has been less useful, as it often doesn't
@@ -358,46 +350,32 @@
 ;;(add-to-list 'load-path
 ;;             "/home/fanner/EmacsIDE/cedet-extra")
 ;;(require 'cedet-m3)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;code completion using auto-complete and ac-python
-;;Following http://utkarshsengar.com/2011/06/emacs-python/
-;;Largely based on getting EVERYTHING via emacs-for-python.
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;(load-file "~/EmacsIDE/emacs-for-python/epy-init.el")
-;; This is needed to stop  garbage instead of a nicely coloured
-;; prompt, as per http://ipython.org/ipython-doc/rel-1.0.0/config/editors.html
-;; (setq ansi-color-for-comint-mode t)
-;; (epy-setup-ipython)
-
-;;Disable some autocompletion bits introduced by emacs-for-python
-;;which annoyingly inserts mathching quotes and brackets
-(setq skeleton-pair nil)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Mode hooks for using different jump-to-definition and jump-to-file
-;;settings for various code modes
-;; TODO - look at semantic vs cscope or semantic + cscope
-;; Jump to definition
-
-;; Get rid of newline binding
-;; (global-unset-key (kbd "C-j"))
-;; Use cscope by default for all programming modes
-(eval-after-load 'prog-mode-hook (lambda ()
-                                   (local-set-key (kbd "C-j") 'cscope-find-this-symbol)))
-
-;; Specialise for Python, because elpy does a better job than cscope
-;; TODO - I want C-J. At present I need to use M-. to get elpy's goto
-;; definition
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Sanitise files before saving
-;;(add-hook 'before-save-hook 'delete-trailing-whitespace)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;</cedet ede>
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Katana development support
+;;<ecb>
+;; ecb - emacs code browser (provides a UI layer on top of cedet and ede)
+;; ecb gives you side bars for navigating directories, files and symbols
+(require 'ecb)
+
+;; Following the ecb README from the source directory
+;;(require 'ecb-autoloads) ;; Switched off if you want to speed-up startup
+
+(setq stack-trace-on-error nil) ;;don’t popup Backtrace window
+(setq ecb-tip-of-the-day nil)
+;;(setq ecb-auto-activate t)
+;;(setq ecb-layout-name "left6")
+;;(setq ecb-options-version "2.40")
+;;(setq ecb-primary-secondary-mouse-buttons (quote mouse-1–mouse-2))
+;;(setq ecb-source-path (quote ("~/")))
+;;</ecb>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;<git>
+;;Configure magit and some home-rolled functionality to help me fetch
+;;and review code from various git repositories.
+
 (require 'magit)
 
 ;; Note to self: To stage individual hunks, you need to use TAB to
@@ -464,7 +442,6 @@
   (magit-status "/workspace/fanner/katana2.0/Apps/Geolib3")
   )
 
-  
 (defun magit-status-geolib ()
   (interactive)
   (magit-status "/workspace/fanner/katana2.0/Apps/Geolib3/")
@@ -484,6 +461,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;<gud gdb>
 ;;GUD/GDB for Katana 2.0
 (defun debug-katana ()
   (interactive)
@@ -492,31 +470,28 @@
   (setq gdb-many-windows t)
   (gdb-restore-windows)
   )
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;</gud gdb>
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Easy window and buffer switching
-
-;; Switch windows
-;; see windmove here http://ergoemacs.org/emacs/emacs_winner_mode.html
-(require 'windmove)
-(global-set-key (kbd "<s-left>") 'windmove-left)
-(global-set-key (kbd "<s-right>") 'windmove-right)
-(global-set-key (kbd "<s-down>") 'windmove-down)
-(global-set-key (kbd "<s-up>") 'windmove-up)
-
-;; Swap buffers
-;; http://stackoverflow.com/questions/1774832/how-to-swap-the-buffers-in-2-windows-emacs
-;; and http://www.emacswiki.org/emacs/TransposeFrame
-(add-to-list 'load-path
-             "~/.emacs.d/extra")
-(require 'transpose-frame)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;<markdown mode>
+;; editing/viewing of markdown files
+;; use git-hub-flavoured markdown instead of standard
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+;; standard markdown flavour is below
+;;(autoload 'markdown-mode "markdown-mode"
+;;  "Major mode for editing Markdown files" t)
+;;; Markdown mode, choosing the github-flavoured-mode, as that gives
+;;; syntax highlighting. Woot!
+(add-to-list 'auto-mode-alist '("\\.text\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
+;; see http://stackoverflow.com/a/18443287
+(setq markdown-command "markdown2 -x fenced-code-blocks -x tables")
+;;</markdown mode>
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Colour themes
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;<colour theme>
 (require 'color-theme)
 (require 'color-theme-solarized)
 (setq color-theme-is-global t)
@@ -526,6 +501,7 @@
                              (progn
                                 (color-theme-initialize)
                                 (color-theme-solarized-dark))))
+;;</colour theme>
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (message "done loading cedet_config.el")
