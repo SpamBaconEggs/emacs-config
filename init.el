6ddf3109dc94e34cae5b565ed08aa953de6ad4f2
SpamBaconEggs <robert.fanner@gmail.com>
2017-05-15 09:10:55 +0200

    Added function to copy words
    C-c w

--- init.el
+++ init.el
@@ -764,6 +764,45 @@
                            (invert-face 'mode-line)
                            (run-with-timer 0.1 nil 'invert-face 'mode-line)))
 ;;</visible bell>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Commands to add copy-word, so you don't have to M-d, C-y, C-x s (phew!)
+;; See https://www.emacswiki.org/emacs/CopyWithoutSelection
+(defun get-point (symbol &optional arg)
+  "get the point"
+  (funcall symbol arg)
+  (point)
+  )
+
+(defun copy-thing (begin-of-thing end-of-thing &optional arg)
+   "copy thing between beg & end into kill ring"
+    (save-excursion
+      (let ((beg (get-point begin-of-thing 1))
+         (end (get-point end-of-thing arg)))
+        (copy-region-as-kill beg end)))
+ )
+
+(defun paste-to-mark(&optional arg)
+   "Paste things to mark, or to the prompt in shell-mode"
+   (let ((pasteMe 
+     (lambda()
+       (if (string= "shell-mode" major-mode)
+         (progn (comint-next-prompt 25535) (yank))
+       (progn (goto-char (mark)) (yank) )))))
+    (if arg
+        (if (= arg 1)
+        nil
+          (funcall pasteMe))
+      (funcall pasteMe))
+    ))
+
+(defun copy-word (&optional arg)
+      "Copy words at point into kill-ring"
+       (interactive "P")
+       (copy-thing 'backward-word 'forward-word arg)
+       ;;(paste-to-mark arg)
+       )
+(global-set-key (kbd "C-c w")  'copy-word)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;<server>
 ;; always start emacs as a server so emacsclient can connect to it
